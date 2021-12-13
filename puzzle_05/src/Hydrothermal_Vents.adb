d9f468408803b350fa92f5f355a666937ef31c92
William J. Franck <william@sterna.io>
2021-12-13 22:56:20 +0100

    Good Working Version (Part A and B unified)

--- puzzle_05/src/Hydrothermal_Vents.adb
+++ puzzle_05/src/Hydrothermal_Vents.adb
@@ -4,23 +4,63 @@ package body Hydrothermal_Vents is
        (Map : in out Chart;
         Seg :        Segment)
     is
+        AB_Distance_X, AB_Distance_Y : Integer;
     begin
+        AB_Distance_X := Seg.B.X - Seg.A.X;
+        AB_Distance_Y := Seg.B.Y - Seg.A.Y;
         -- case vertical lines
-        if Seg.A.X = Seg.B.X then
-            for y in Seg.A.Y .. Seg.B.Y loop
-                Map (Seg.A.X, y) := @ + 1;
-            end loop;
-            for y in Seg.B.Y .. Seg.A.Y loop
-                Map (Seg.A.X, y) := @ + 1;
-            end loop;
+        if AB_Distance_X = 0 then
+            if AB_Distance_Y >= 0 then
+                for y in 0 .. AB_Distance_Y loop
+                    Map (Seg.A.X, Seg.A.Y + y) := @ + 1;
+                end loop;
+            end if;
+            if AB_Distance_Y < 0 then
+                for y in 0 .. abs (AB_Distance_Y) loop
+                    Map (Seg.A.X, Seg.A.Y - y) := @ + 1;
+                end loop;
+            end if;
+
             -- case horizontal lines
-        elsif Seg.A.Y = Seg.B.Y then
-            for x in Seg.A.X .. Seg.B.X loop
-                Map (x, Seg.A.Y) := @ + 1;
-            end loop;
-            for x in Seg.B.X .. Seg.A.X loop
-                Map (x, Seg.A.Y) := @ + 1;
-            end loop;
+        elsif AB_Distance_Y = 0 then
+            if AB_Distance_X >= 0 then
+                for x in 0 .. AB_Distance_X loop
+                    Map (Seg.A.X + x, Seg.A.Y) := @ + 1;
+                end loop;
+            end if;
+            if AB_Distance_X < 0 then
+                for x in 0 .. abs (AB_Distance_X) loop
+                    Map (Seg.A.X - x, Seg.A.Y) := @ + 1;
+                end loop;
+            end if;
+
+            -- case diagonal lines
+        elsif abs (AB_Distance_X) = abs (AB_Distance_Y) then
+
+            if AB_Distance_X > 0 and AB_Distance_Y > 0 then
+                for i in 0 .. AB_Distance_X loop
+                    Map (Seg.A.X + i, Seg.A.Y + i) := @ + 1;
+                end loop;
+            end if;
+
+            if AB_Distance_X < 0 and AB_Distance_Y < 0 then
+                for i in 0 .. abs (AB_Distance_X) loop
+                    Map (Seg.A.X - i, Seg.A.Y - i) := @ + 1;
+                end loop;
+            end if;
+
+            if AB_Distance_X < 0 and AB_Distance_Y > 0 then
+                for i in 0 .. abs (AB_Distance_X) loop
+                    Map (Seg.A.X - i, Seg.A.Y + i) := @ + 1;
+                end loop;
+            end if;
+
+            if AB_Distance_X > 0 and AB_Distance_Y < 0 then
+                for i in 0 .. AB_Distance_X loop
+                    Map (Seg.A.X + i, Seg.A.Y - i) := @ + 1;
+                end loop;
+            end if;
+
         end if;
 
     end set;
